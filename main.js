2225e7c6cc633c32bfe3ff1e576f0ea9b8203613
Cotamor <cotamor@gmail.com>
2024-02-14 15:47:47 +0900

    Finished pet filtering

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const template = document.querySelector('#pet-card-template')
 const wrapper = document.createDocumentFragment()
 
+// Display Current Temperature
 async function start() {
   const weatherPromise = await fetch(
     'https://api.weather.gov/gridpoints/TBW/109,102/forecast'
@@ -20,10 +21,10 @@ async function start() {
   const tempOutput = document.querySelector('#temp-output')
   tempOutput.textContent = convertedToC
 }
-
 start()
 
-async function petsArea() {
+// Pets Area
+async function petsArea(species) {
   const petsPromise = await fetch(
     'https://learnwebcode.github.io/bootcamp-pet-data/pets.json'
   )
@@ -31,6 +32,9 @@ async function petsArea() {
 
   petsData.forEach((pet) => {
     const clone = template.content.cloneNode(true)
+
+    clone.querySelector('.pet-card').dataset.species = pet.species
+
     clone.querySelector('h3').textContent = pet.name
     clone.querySelector('.pet-description').textContent = pet.description
     clone.querySelector('.pet-age').textContent = createAgeText(pet.birthYear)
@@ -44,8 +48,8 @@ async function petsArea() {
   })
   document.querySelector('.list-of-pets').appendChild(wrapper)
 }
+petsArea('all')
 
-petsArea()
 function createAgeText(birthYear) {
   const currentYear = new Date().getFullYear()
   const age = currentYear - birthYear
@@ -67,5 +71,13 @@ function handleButtonClick(e) {
   allButtons.forEach((el) => el.classList.remove('active'))
   // add active class to the specific button that just got clicked
   e.target.classList.add('active')
-  // filter the pets down below
+  // filter the pets
+  const currentFilter = e.target.dataset.filter
+  document.querySelectorAll('.pet-card').forEach((el) => {
+    if (el.dataset.species == currentFilter || currentFilter == 'all') {
+      el.style.display = 'grid'
+    } else {
+      el.style.display = 'none'
+    }
+  })
 }
